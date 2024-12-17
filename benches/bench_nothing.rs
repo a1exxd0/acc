7b4640af131bcbe9c4ebbadc205ff89a0b3b14fb
Alex Do <alexxdo2004@gmail.com>
2024-12-17 09:15:36 +0000

    feat: add extra example benchmark

--- benches/bench_nothing.rs
+++ benches/bench_nothing.rs
@@ -12,5 +12,13 @@ fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
 }
 
-criterion_group!(benches, criterion_benchmark);
+fn factorial(n: u64) -> u64 {
+    (1..=n).product()
+}
+
+fn bench_factorial(c: &mut Criterion) {
+    c.bench_function("factorial 10", |b| b.iter(|| factorial(black_box(10))));
+}
+
+criterion_group!(benches, criterion_benchmark, bench_factorial);
 criterion_main!(benches);
