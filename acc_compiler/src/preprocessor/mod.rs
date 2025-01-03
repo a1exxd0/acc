875aa31801c7291eb671be08dadf388b2acfef48
Alex Do <alexxdo2004@gmail.com>
2025-01-03 00:56:04 +0000

    docs: add description for character_mapping module

--- acc_compiler/src/preprocessor/mod.rs
+++ acc_compiler/src/preprocessor/mod.rs
@@ -1,2 +1,5 @@
+/// Deals with translation phases 1 and 2:
+/// 1) Character mapping to source set
+/// 2) Concatenation of backslash-newlines into one line
 pub mod character_mapping;
 pub mod lexer;
